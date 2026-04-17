fe1c1af793a58ff42ce77154e0faf680d65060e9
ashutoshrana20 <143320470+ashutoshrana20@users.noreply.github.com>
2026-04-17 12:54:53 +0530

    commit

---
+++ index.js
@@ -0,0 +1,167 @@
+// console.log("hello world")
+
+// console.log(" Hello Ashu")
+
+// let age = 20;
+// if(age>=52){
+//     console.log("Adult")
+// }else if(age>=65){
+//     console.log("Minor")
+// }else{
+//     console.log("asdf")
+// }
+
+
+// 1. Declare a variable name and assign your name. Print it
+// let name = "Rohit";
+// console.log(name);
+// output = Rohit
+
+// 2. Declare a variable age using let and assign value 25.
+//  let age = 25;
+//  console.log(age);
+// output = 25
+
+// 3. Declare a constant country and assign value "India".
+// var country = "India";
+// console.log(country)
+// output = India
+
+// 4. Change value of let variable and print before and after.
+//   let age = 32;
+//  console.log("Befor",age);
+//   age = 40;
+//  console.log("After", age);
+//  output = Befor 32
+//  output = After 40
+
+// 5. Try changing const variable value. What happens?
+// let age = 30;
+// console.log(age);
+// output 30
+ 
+//====== Create variables with data types:=====
+
+// 6. Print typeof of string variable.
+  // let name  = "Ashu"
+  // console.log(typeof name) 
+ // output = string 
+
+  // 7.Print typeof of number variable.
+  // var age = 36;
+  // console.log(typeof age);
+  // output = number
+
+  // 8. Print typeof of boolean variable.
+    //  const t = true;
+    //  console.log(typeof t);
+    // output = Boolean
+
+    // let f = false;
+    // console.log(typeof f);
+    // output= Boolean
+  // 9.Print typeof of boolean variable.
+    // const t = false;
+    // console.log(typeof t);
+    // output = Boolean
+  
+
+    // 10. Print typeof of null and explain output.
+    // let N = null;
+    // console.log( typeof N);
+    // output = object
+
+    // 11. typeof "Hello"
+    //  let h = "Hello "
+    //  console.log( typeof "hello");   
+    // output = string
+
+    // Q12 typeof 100
+      // let  z = 100;
+      // console.log(typeof z);  
+     
+      // z = 200;
+      // console.log( typeof z);
+      //  output = number
+
+  //  13. typeof true
+    // const t = true;
+    // console.log(typeof t); 
+
+    // 14. typeof undefined
+    //   let U;
+    //  console.log(U);
+    //  output = undefined
+
+    // 15. typeof null
+    // let n = null;
+    // console.log(typeof n);
+    // output = object
+
+    // ====🟢 PART 4 – Operators (Q16–Q25)=====
+
+    // 16. console.log(10 + 5)
+    // x = 10;
+    // y = 5;
+    // console.log(x+y);
+    // output  = 15
+
+    //17. console.log(10 - 5)
+    //  let a = 10;
+    //  let b = 5;
+    //  console.log(10-5);
+    //  output = 5
+
+    // 18. console.log(10 * 5)
+        // q = 10;
+        // w = 5;
+        // console.log(10*5);
+        // output = 50;
+      
+    // 19. console.log(10 / 2)
+    //  e = 10;
+    //  f = 2;
+    //  console.log(10/2) 
+    //  output = 5
+
+  //20. console.log(10 % 3)
+  //  let d = 10;
+  //  var g = 3;
+  //  console.log(10%3);
+  //  output = 1 
+
+  // 21.console.log(5 == "5")
+    // m = 5;
+    // n = 5;
+    // console.log(5=="5");
+    // output = true
+
+    // l = 5;
+    // k = "5"
+    // console.log(5==="5")
+    strict 
+    // output = false
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+
+
+
+
+
+
+  
+
