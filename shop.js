44d563ce6c3393ad24adfb7249af266504482985
ashutoshrana20 <143320470+ashutoshrana20@users.noreply.github.com>
2026-06-17 10:51:00 +0530

    dom

--- shop.js
+++ shop.js
@@ -1,4 +1,3 @@
-
 // let color = document.getElementById("color")
 // let red = document.getElementById("red")
 // let blue = document.getElementById("blue")
@@ -15,12 +14,10 @@
 //     color.style.color = "green";
 // })
 
-
 // let list = document.querySelectorAll("li")
 // for(let i=0;i<list.length;i++){
 //     console.log(list[i].textContent)
 // }
-
 
 // let para = document.querySelectorAll("p")
 // let h1 = document.querySelectorAll("h1")
@@ -35,8 +32,6 @@
 
 // let p = document.querySelector("p")
 
-
-
 // function dblclick(){
 //     p.style.color = "yellow";
 // }
@@ -48,18 +43,72 @@
 
 // input.addEventListener("input",function(helo){
 //    console.log(helo.data)
-   
+
 // })
 
+// let h1 = document.querySelector("h1")
 
-let h1 = document.querySelector("h1")
+//  window.addEventListener("keydown",function(details){
+//     h1.textContent = details.key;
+//     if(details.key === " "){
+//         h1.textContent = "spc"
+//     }
+//     else{
+//         h1.textContent = details.key;
+//     }
+// })
 
- window.addEventListener("keydown",function(details){
-    h1.textContent = details.key;
-    if(details.key === " "){
-        h1.textContent = "spc"
-    }
-    else{
-        h1.textContent = details.key;
-    }
-})
+// let form = document.querySelector ('form');
+// let inputs = document.querySelectorAll ('input');
+// let main = document.querySelector("#main")
+
+// form.addEventListener ('submit', function (details) {
+//   details.preventDefault ();
+
+//   let card = document.createElement ('div');
+//   card.classList.add ('card');
+
+//   let profile = document.createElement ('div');
+//   profile.classList.add ('profile');
+
+//   let img = document.createElement ('img');
+//   img.setAttribute (
+//     'src',
+//     'https://images.unsplash.com/photo-1773332611550-9f5a465b4e42?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2NHx8fGVufDB8fHx8fA%3D%3D'
+//   );
+
+
+//   let h3 = document.createElement("h3")
+//   h3.textContent = "ghoda ghodi";
+//   let h5 = document.createElement("h5");
+//    h5.textContent = "Ghoda";
+//   let p = document.createElement("p")
+//   p.textContent = "ghoda is very good"
+
+
+//   profile.appendChild("img");
+//   card.appendChild("profile")
+
+
+//   card.appendChild(h3)
+//   card.appendChild(profile)
+//   card.appendChild(p)
+
+
+//   card.appendChild (h5);
+//   card.appendChild (p);
+
+//   main.appendChild (card);
+
+//   card.appendChild (profile);
+//   console.log (card);
+
+//   inputs.forEach (function (inp) {
+//     if (inp.type !== 'submit') {
+//       inp.value = '';
+//     }
+//   });
+// });
+
+
+document.querySelector("")
